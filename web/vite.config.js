e385b1a6a162a080e9445b1ec10303df06e324e0
Nicocchi <nicocchilover@gmail.com>
2024-02-22 05:29:19 -0500

    updated vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 44091
+    port: 44091,
+  },
+  base: "",
+  resolve: {
+    alias: {
+      $font: resolve('./public/font')
+    }
   }
 })
